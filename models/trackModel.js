9af0af7ad0be9773461d34057c52157a3be7259d
Franklin Ledezma <fledezmachavez@live.com>
2026-01-01 11:58:02 -0600

    Adding track model and Mapper

---
+++ models/trackModel.js
@@ -0,0 +1,9 @@
+export const track = {
+    id:undefined, 
+    title:undefined, 
+    artistsText:undefined, 
+    albumTitle:undefined, 
+    albumImageUrl:undefined, 
+    durationMs:undefined, 
+    spotifyUrl:undefined,
+}
