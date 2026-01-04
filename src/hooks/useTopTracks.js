b80a6e69862a317ac3bccfd98385206ca7e365af
Franklin Ledezma <fledezmachavez@live.com>
2026-01-04 14:07:02 -0600

    Adding basic cache for topTracks

--- src/hooks/useTopTracks.js
+++ src/hooks/useTopTracks.js
@@ -1,22 +1,32 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { getTracks } from "../spotifyAPI";
 import { spotifyRawToTrack } from "../../models/trackMapper";
 
 
 const DEFAULT_LIMIT = 5;
 
+
 export default function useTopTracks() {
     const [topTracks, setTopTracks] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
+    const cacheTopTracks = useRef(new Map())
 
     async function loadTopTracks(limit = DEFAULT_LIMIT) {
         setError(null);
         setLoading(true);
 
+        if (cacheTopTracks.current.has(limit)) {
+            let cachedTracks = cacheTopTracks.current.get(limit)
+            setTopTracks([...cachedTracks])
+            setLoading(false);
+            return
+        }
+
         try {
             const spotifyTracks = await getTracks(limit);
-            const tracks = spotifyTracks.map(st=> spotifyRawToTrack(st.track));
+            const tracks = spotifyTracks.map(st => spotifyRawToTrack(st.track));
+            cacheTopTracks.current.set(limit, tracks)
             setTopTracks(tracks);
         } catch (e) {
             setTopTracks([]);
@@ -26,5 +36,8 @@ export default function useTopTracks() {
         }
     }
 
-    return { topTracks, loadTopTracks, error, loading };
+
+
+return { topTracks, loadTopTracks, error, loading };
 }
+
